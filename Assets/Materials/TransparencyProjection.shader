2aa83f3734de0737c06047c3b76c6deeb4c2e60e
Elisha Azaria <elishadar@gmail.com>
2020-12-28 23:38:12 +0200

    Added Fog Of War

---
+++ Assets/Materials/TransparencyProjection.shader
@@ -0,0 +1,78 @@
+﻿Shader "Custom/TransparencyProjection"
+{
+    Properties
+    {
+        _PrevTexture ("Previous Texture", 2D) = "white" {}
+        _CurrTexture ("Current Texture", 2D) = "white" {}
+
+        _MainTexture ("Main Texture",2D) = "white"{}
+
+        //        _Color ("Color", Color) = (0, 0, 0, 0)
+        //        _Alpha ("Alpha", float) = 0
+        _Blend("Blend", Float) = 0
+    }
+    SubShader
+    {
+        Tags
+        {
+            "Queue"="Transparent+100"
+        } // to cover other transparent non-z-write things
+
+        Pass
+        {
+            ZWrite Off
+            Blend SrcAlpha OneMinusSrcAlpha
+            ZTest Equal
+
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float4 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float4 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+            };
+
+
+            float4x4 unity_Projector;
+            sampler2D _CurrTexture;
+            sampler2D _PrevTexture;
+            // fixed4 _Color;
+            float _Blend;
+
+            sampler2D _MainTexture;
+
+            v2f vert(appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = mul(unity_Projector, v.vertex);
+                return o;
+            }
+
+            fixed4 frag(v2f i) : SV_Target
+            {
+                float aPrev = tex2Dproj(_PrevTexture, i.uv).a;
+                float aCurr = tex2Dproj(_CurrTexture, i.uv).a;
+
+                fixed4 col = tex2Dproj(_MainTexture, i.uv);
+
+                fixed a = lerp(aPrev, aCurr, _Blend);
+
+                // weird things happen to minimap if alpha value gets negative
+                col.a = max(0, 1 - a);
+                return col;
+            }
+            ENDCG
+        }
+    }
+}
---
+++ Assets/Scripts/Player/FogProjector.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class FogProjector : MonoBehaviour
+{
+	public Material projectorMaterial;
+	public float blendSpeed;
+	public int textureScale;
+
+	public RenderTexture fogTexture;
+
+	private RenderTexture prevTexture;
+	private RenderTexture currTexture;
+	private Projector projector;
+
+	private float blendAmount;
+	private static readonly int PrevTexture = Shader.PropertyToID("_PrevTexture");
+	private static readonly int CurrTexture = Shader.PropertyToID("_CurrTexture");
+	private static readonly int Blend = Shader.PropertyToID("_Blend");
+
+	private void Awake()
+	{
+		// fogTexture.format = RenderTextureFormat.ARGB32;
+		projector = GetComponent<Projector>();
+		projector.enabled = true;
+
+		prevTexture = GenerateTexture();
+		currTexture = GenerateTexture();
+
+		// Projector materials aren't instanced, resulting in the material asset getting changed.
+		// Instance it here to prevent us from having to check in or discard these changes manually.
+		projector.material = new Material(projectorMaterial);
+
+		projector.material.SetTexture(PrevTexture, prevTexture);
+		projector.material.SetTexture(CurrTexture, currTexture);
+
+		StartNewBlend();
+	}
+
+	RenderTexture GenerateTexture()
+	{
+		RenderTexture rt = new RenderTexture(
+			fogTexture.width * textureScale,
+			fogTexture.height * textureScale,
+			0,
+			fogTexture.format);
+		rt.filterMode = FilterMode.Bilinear;
+		rt.antiAliasing = fogTexture.antiAliasing;
+
+		return rt;
+	}
+
+	public void StartNewBlend()
+	{
+		StopCoroutine(BlendFog());
+		blendAmount = 0;
+		// Swap the textures
+		Graphics.Blit(currTexture, prevTexture);
+		Graphics.Blit(fogTexture, currTexture);
+
+		StartCoroutine(BlendFog());
+	}
+
+	IEnumerator BlendFog()
+	{
+		while (blendAmount < 1)
+		{
+			// increase the interpolation amount
+			blendAmount += Time.deltaTime * blendSpeed;
+			// Set the blend property so the shader knows how much to lerp
+			// by when checking the alpha value
+			projector.material.SetFloat(Blend, blendAmount);
+			yield return null;
+		}
+
+		// once finished blending, swap the textures and start a new blend
+		StartNewBlend();
+	}
+}
