1994dc036a9fcffff858618b0e90c208260f481e
Elisha Azaria <elishadar@gmail.com>
2020-12-30 01:01:35 +0200

    Changed to TextMeshPro

--- Assets/Scripts/Controllers/EnemySoldierController.cs
+++ Assets/Scripts/Controllers/EnemySoldierController.cs
@@ -5,8 +5,22 @@ using UnityEngine;
 
 public class EnemySoldierController : SoldierController
 {
+	private static readonly List<EnemySoldierController> enemies = new List<EnemySoldierController>();
+
+	private void Awake()
+	{
+		enemies.Add(this);
+	}
+
+	private void OnDestroy()
+	{
+		enemies.Remove(this);
+	}
+
+
 	private void OnGUI()
 	{
-		GUILayout.Label($"HP: {life.hp}");
+		// GUILayout.Label($"HP: {life.hp}");
+		GUILayout.Label($"Enemies Left: {enemies.Count}");
 	}
 }
