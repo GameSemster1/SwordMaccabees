d118efb753397cf5ce2abd0d153f84e727d37c46
emmanuel405 <57897803+emmanuel405@users.noreply.github.com>
2020-12-30 00:39:26 +0200

    fin

---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    [SerializeField] float sphereradius = 0.2f;
+    [SerializeField] Color sphereColor = Color.red;
+
+    private void OnDrawGizmos(){
+        Gizmos.color = sphereColor;
+        Gizmos.DrawSphere(transform.position, sphereradius);
+    }
+}
---
+++ Assets/Scripts/myText.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class myText : MonoBehaviour
+{
+ public string canvasText1 ="jj";
+   void Start(){
+        Invoke("DisableText", 5f); //invoke after 5 seconds
+   }
+   void DisableText(){ 
+        canvasText1 = "";
+   }    
+}
+
