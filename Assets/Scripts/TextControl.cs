71b2ebfa70ad85ddd6d83b28d991611f82e07b3d
Elisha Azaria <elishadar@gmail.com>
2021-01-01 14:04:32 +0200

    Fixed problems with text

---
+++ Assets/Scripts/TextControl.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class TextControl : MonoBehaviour
+{
+	[SerializeField] private GameObject[] list;
+	[SerializeField] private KeyCode next = KeyCode.Space;
+
+	private int currentIndex = 0;
+
+	private void Start()
+	{
+		for (var i = 0; i < list.Length; i++)
+		{
+			list[i].SetActive(i == currentIndex);
+		}
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown(next) && currentIndex < list.Length)
+		{
+			currentIndex++;
+			for (var i = 0; i < list.Length; i++)
+			{
+				list[i].SetActive(i == currentIndex);
+			}
+		}
+	}
+}
